a846e137ad420ebc4d1ab1ba1a8291164ce32500
Valeriy <val.batyukov@gmail.com>
2026-02-03 23:22:21 +0300

    Refactor: add js modules structure for web app

--- run_api.py
+++ run_api.py
@@ -1,10 +1,10 @@
 import os
 import uvicorn
 
-if __name__ == "__main__":  # Двойные подчеркивания!
+if __name__ == "__main__":  # Добавьте двойные подчеркивания!
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(
-        "main:app",  # Без префикса "app."
+        "app.main:app",  # Правильный путь к файлу внутри папки app
         host="0.0.0.0", 
         port=port, 
         reload=False
